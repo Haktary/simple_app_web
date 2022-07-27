89e83c57aea3dc0e8e34e6a520d6895a167e1b63
root <root@debian>
2022-07-27 17:27:43 +0200

    docs code

--- views/add_message.php
+++ views/add_message.php
@@ -5,10 +5,12 @@ function add_message()
 {
   global $conn;
   $envoie = True;
+  // get post 
   $nom = $_POST["nom"];
   $email = $_POST["email"]; 
   $tel = $_POST["tel"];
   $message = $_POST["message"];
+  /* integriter des donnee email et tel */ 
   if (!preg_match('/^\\S+@\\S+\\.\\S+$/', $email)) {
     $envoie = False ;
     $response=array(
@@ -22,26 +24,26 @@ function add_message()
       'erreur' => 'tel erreur de saisie'
     );
   }
-
+  /* enlevée les balise (protection xss) */ 
   $nom = strip_tags($nom);
   $email = strip_tags($email);
   $tel = strip_tags($tel);
   $message = strip_tags($message);
 
-
-  
+  /* si les data son intègre */ 
   if ($envoie == True) {
-    #$query="INSERT INTO message(nom, email, tel, message) VALUES('".$nom."', '".$email."', '".$tel."', '".$message."')";
+    // prep de la sql (protection sql)
     $stmt = $conn->prepare("INSERT INTO message(nom, email, tel, message) VALUES(?, ?, ?, ?)");
     $stmt->bind_param("ssss", $nom, $email, $tel, $message);
     
     if ($stmt->execute()) {
-      // config your server smtp 
+      // function mail need to config your server smtp 
       $to = "info@bulko.net";
       $subject = "receive message";
       $txt = $nom . $email . $tel . $message ; 
       $headers = "From: webmaster@example.com";
-      $mail = mail($to,$subject,$txt,$headers);
+      // envoie du mail 
+      $mail = mail($to,$subject,$txt,$headers); 
       $stmt->close();
       $response=array(
         'status' => 1,
